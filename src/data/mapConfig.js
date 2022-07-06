de6f739413aa84f087b3ada39804e955534dea0d
UnusualMessage <nikita_kot2@mail.ru>
2022-07-06 13:01:43 +0300

    Add urls and ids default values to config

--- src/data/mapConfig.js
+++ src/data/mapConfig.js
@@ -2,5 +2,12 @@ import {fromLonLat} from "ol/proj";
 
 const defaultCenter = fromLonLat([-77.018627, 38.899924]);
 const defaultZoom = 15;
+const defaultVisibility = true;
 
-export { defaultZoom, defaultCenter };
+const geoJsonId = "geo";
+const csvId = "csv";
+
+const geoJsonUrl = "bars.geojson";
+const csvUrl = "portals.csv";
+
+export { defaultZoom, defaultCenter, defaultVisibility, geoJsonId, csvId, geoJsonUrl, csvUrl };
